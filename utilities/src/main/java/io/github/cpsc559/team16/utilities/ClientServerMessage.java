57477ff19299a5698c5721d0f6cc9d5ded533fab
briggscole <cole.a.briggs@gmail.com>
2025-03-17 11:34:29 -0600

    Added clientCounter for ack mechanism
    Client will compare clientCounters when receiving their own message back to determine specifically which message has been successfully delivered

--- utilities/src/main/java/io/github/cpsc559/team16/utilities/ClientServerMessage.java
+++ utilities/src/main/java/io/github/cpsc559/team16/utilities/ClientServerMessage.java
@@ -2,14 +2,16 @@ package io.github.cpsc559.team16.utilities;
 
 public class ClientServerMessage extends BaseMessage {
     private String content;
+    private int clientCounter;
 
     public ClientServerMessage() {
         super();
     }
 
-    public ClientServerMessage(String sender, String receiver, String content) {
+    public ClientServerMessage(String sender, String receiver, String content, int clientCounter) {
         super(sender, receiver);
         this.content = content;
+        this.clientCounter = clientCounter;
     }
 
     public String getContent() {
@@ -18,6 +20,14 @@ public class ClientServerMessage extends BaseMessage {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public int getClientCounter() {
+        return clientCounter;
+    }
+
+    public void setClientCounter(int clientCounter) {
+        this.clientCounter = clientCounter;
     }
 
     @Override
