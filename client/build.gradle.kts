e180536fed6c24f501999ab8f3f527bbe1bb6164
briggscole <cole.a.briggs@gmail.com>
2025-03-17 11:42:01 -0600

    Configured jline 3.21.0 import (for UI)

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
 dependencies {
     implementation(project(":utilities"))                                   // Adds dependency on the utilities module
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")    // JSON support
+    implementation("org.jline:jline:3.21.0")                                // JLine for console input/output
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")            // JUnit 5 testing framework
 }
 
