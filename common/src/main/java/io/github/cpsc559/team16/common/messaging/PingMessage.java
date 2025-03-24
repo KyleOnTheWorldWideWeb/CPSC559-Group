a875d1b152e7869d2696a9439325c3ac393ab34c
briggscole <cole.a.briggs@gmail.com>
2025-03-24 17:18:34 -0600

    Created PingMessage

--- common/src/main/java/io/github/cpsc559/team16/common/messaging/PingMessage.java
+++ common/src/main/java/io/github/cpsc559/team16/common/messaging/PingMessage.java
@@ -1,4 +1,32 @@
 package io.github.cpsc559.team16.common.messaging;
 
-public class PingMessage  extends BaseAddrServerMessage {
+/**
+ * A standardized ping message used to broadcase the availability of the primary {@link AddressingServer}.
+ *
+ * <p>
+ * The ping is sent by the primary to all replicas, indicating that it is still alive.
+ * </p>
+ * 
+ * <pre>
+ * Example:
+ * {
+ *   "msgType": "PING",
+ *   "objectType": "NONE",
+ *   "senderPID": 11,
+ *   "senderRole": "",
+ *   "targetRole": "",
+ *   "payload": ""
+ * }
+ * </pre>
+ */
+public class PingMessage extends BaseAddrServerMessage<String> {
+
+    /**
+     * Constructs a ping message, used by the primary to indicate that it is still alive.
+     *
+     * @param senderPID The ID of the process sending the ping.
+     */
+    private PingMessage(long senderPID) {
+        super(MessageTypes.ELECTION, ObjectTypes.NONE, senderPID, "", "", "");
+    }
 }
