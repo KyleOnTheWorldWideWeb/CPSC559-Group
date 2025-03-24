0ffb1b1dfcd4ea6fac6a5c3b766178b8081c19f4
briggscole <cole.a.briggs@gmail.com>
2025-03-24 13:07:54 -0600

    Setup ElectionMessage with methods for generating election, bully, and leader messages

--- common/src/main/java/io/github/cpsc559/team16/common/messaging/ElectionMessage.java
+++ common/src/main/java/io/github/cpsc559/team16/common/messaging/ElectionMessage.java
@@ -1,4 +1,65 @@
 package io.github.cpsc559.team16.common.messaging;
 
-public class ElectionMessage {
+/**
+ * A standardized election message used for initiating a leader election.
+ *
+ * <p>
+ * The payload of an {@code ElectionMessage} is a string (e.g., "Election", "Bully", "Leader").
+ * </p>
+ * 
+ * <pre>
+ * Example:
+ * {
+ *   "msgType": "ELECTION",
+ *   "objectType": "",
+ *   "senderPID": 11,
+ *   "senderRole": "",
+ *   "targetRole": "",
+ *   "payload": "Leader"
+ * }
+ * </pre>
+ */
+public class ElectionMessage extends BaseAddrServerMessage<String> {
+
+    /**
+     * Constructs a new election message, used to initiate a leader election.
+     *
+     * @param senderPID The ID of the process initiating the election.
+     */
+    private ElectionMessage(long senderPID, String payload) {
+        super("ELECTION", "String", senderPID, "", "", payload);
+    }
+
+    /**
+     * Creates a simple "ELECTION" message used to initiate
+     * a leader election with the senderPID running.
+     *
+     * @param senderPID The process ID of the sender.
+     * @return A basic {@code ElectionMessage} with payload "Election".
+     */
+    public static ElectionMessage election(long senderPID){
+        return new ElectionMessage(senderPID, "Election");
+    }
+
+    /**
+     * Creates a simple "BULLY" message used to respond to an
+     * "ELECTION" message from a sender with a lower PID.
+     *
+     * @param senderPID The process ID of the sender.
+     * @return A basic {@code ElectionMessage} with payload "Bully".
+     */
+    public static ElectionMessage bully(long senderPID){
+        return new ElectionMessage(senderPID, "Bully");
+    }
+
+    /**
+     * Creates a simple "LEADER" message used to announce a
+     * leader election winner.
+     *
+     * @param senderPID The process ID of the sender.
+     * @return A basic {@code ElectionMessage} with payload "Leader".
+     */
+    public static ElectionMessage leader(long senderPID){
+        return new ElectionMessage(senderPID, "Leader");
+    }
 }
