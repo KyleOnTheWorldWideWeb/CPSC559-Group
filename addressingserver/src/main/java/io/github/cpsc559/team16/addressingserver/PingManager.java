93dc87be99a83a729dc3e465a0c8e4a6279cb622
briggscole <cole.a.briggs@gmail.com>
2025-03-24 14:49:43 -0600

    Setup runnable PingManager to handle to ping timeout

--- addressingserver/src/main/java/io/github/cpsc559/team16/addressingserver/PingManager.java
+++ addressingserver/src/main/java/io/github/cpsc559/team16/addressingserver/PingManager.java
@@ -1,5 +1,69 @@
 package io.github.cpsc559.team16.addressingserver;
 
-public class PingManager {
+public class PingManager implements Runnable {
+
+    /**
+     * Flag used to mark whether the process should shutdown
+     */
+    private boolean shutdown = false;
+    public void shutdown() {
+        shutdown = true;
+    }
+
+    /**
+     * The {@code LeaderElectionManager} instance used to initiate elections
+     */
+    private final LeaderElectionManager leaderElectionManager;
+
+    /**
+     * Flag used to mark whether a ping has been received (since its last reset)
+     */
+    private boolean receivedPing;
+
+    /**
+     * Timeout duration to wait for ping
+     */
+    private int pingTimeout = 3000; // Milliseconds
+    public void setPingTimeout(int milliseconds) {
+        pingTimeout = milliseconds;
+    }
+
+    public PingManager(LeaderElectionManager leaderElectionManager) {
+        this.leaderElectionManager = leaderElectionManager;
+        receivedPing = false;
+    }
+
+    /**
+     * Notify this process that a ping has been received
+     */
+    public void ping() {
+        receivedPing = true;
+    }
+
+    /**
+     * Wait for timeout, then check if ping has been received.
+     */
+    public void awaitPing() {
+
+        try {
+
+            Thread.sleep(pingTimeout);
+
+            if (!receivedPing) {
+                leaderElectionManager.initiateElection();
+            } else {
+                receivedPing = false;
+            }
+
+        } catch (InterruptedException e) {
+            System.err.println("Ping manager interrupted while awaiting ping.");
+        }
+    }
+
+    public void run() {
+        while (!shutdown) {
+            awaitPing();
+        }
+    }
     
 }
