937f12b76d3b4b9ad0c8bbb32b35e11167b8a654
briggscole <cole.a.briggs@gmail.com>
2025-03-29 13:59:18 -0600

    Added INCORRECT_PASSWORD AckType for responding to client authentication attempts

--- common/src/main/java/io/github/cpsc559/team16/common/messaging/AckTypes.java
+++ common/src/main/java/io/github/cpsc559/team16/common/messaging/AckTypes.java
@@ -33,6 +33,14 @@ public class AckTypes {
      * </p>
      */
     public static final String NOHOST = "NoHost";
+    /**
+     * Used to send an acknowledgement to a Client that their login attempt was unsuccessful.
+     * <p>
+     * This acknowledgment is sent to a Client following their initial connection request to the AddressingServer.
+     * </p>
+     */
+    public static final String INCORRECT_PASSWORD = "IncorrectPassword";
+
 
     public static final String DEMOTED = "Demoted";
 
