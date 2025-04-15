2c4b47a22ee16225fee473d93678f4f6b085a103
briggscole <cole.a.briggs@gmail.com>
2025-04-15 16:54:05 -0600

    MessageUtils provides methods for creating + sending messages

---
+++ client/src/main/java/io/github/cpsc559/team16/client/MessageUtils.java
@@ -0,0 +1,61 @@
+package io.github.cpsc559.team16.client;
+
+import io.github.cpsc559.team16.common.utilities.ClientServerMessage;
+
+/**
+ * Utility class for creating and sending messages in the chat client.
+ * <p>
+ * This class provides methods to create new messages with unique IDs and send
+ * them to the server.
+ * It handles message creation, sending, and error handling during the message
+ * transmission process.
+ * </p>
+ */
+public class MessageUtils {
+
+    private Client client;
+
+    public MessageUtils(Client client) {
+        this.client = client;
+    }
+
+    /**
+     * Creates a new chat message with a unique ID.
+     * The message is created with:
+     * - Current timestamp
+     * - Incremented message counter
+     * - Specified content and receiver
+     * 
+     * @param msgContents The content of the message to send
+     * @param receiver    The intended recipient of the message (e.g., "fellow
+     *                    clients" for broadcast)
+     * @return A new ClientServerMessage with a unique ID and current timestamp
+     */
+    public ClientServerMessage createMessage(String msgContents, String receiver) {
+        Client.debug(Client.DEBUG_DETAILED, "Creating message for receiver: " + receiver);
+        return new ClientServerMessage(client.getUsername(), receiver, client.getNextSendCounter(), msgContents);
+    }
+
+    /**
+     * Sends a message to the server.
+     * If the message fails to send:
+     * - It is added to the message queue for retry
+     * - A reconnection attempt is triggered
+     * - The error is logged at DEBUG_NORMAL level
+     * 
+     * @param msg The message to send to the server
+     * @throws IllegalStateException if the client is not connected to the server
+     */
+    public void sendMessage(ClientServerMessage msg) {
+        try {
+            Client.debug(Client.DEBUG_LOW_LEVEL, "Sending message: " + msg.toJson());
+            System.out.println("Sent" + msg.toJson());
+            client.getOut().println(msg.toJson());
+        } catch (Exception e) {
+            Client.debug(Client.DEBUG_NORMAL, "Error sending message: " + e.getMessage());
+            client.getMessageQueue().add(msg);
+            client.getConnectionManager().reconnect();
+        }
+    }
+    
+}
